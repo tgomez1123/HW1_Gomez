c58a2e9b5009d624111aef2409547e325c0bc224
Thaina Gomez <thaina.yannet.gomez@gmail.com>
2025-02-12 16:36:23 -0500

    Compute the average

--- HW_Gomez.R
+++ HW_Gomez.R
@@ -1,2 +1,5 @@
 random_numbers <- rnorm(100, mean = 25, sd=1
+mean_value <-mean(random_numbers)
+sd_value <- sd(random_numbers)
+print(paste("The mean is", mean_value, "and the standard deviation is", sd_value))
 
